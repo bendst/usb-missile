d4cc3ecfb5145ccb6378c03163a4d3c2f6e5e382
Ben Stuart <mail@bstuart.de>
2016-07-28 11:46:53 +0200

    Stil

--- usb-missile.c
+++ usb-missile.c
@@ -10,37 +10,26 @@
 #define VENDOR_ID 0x2123
 #define PRODUCT_ID 0x1010
 
-#define LED_PREFIX 0x03
-#define MV_PREFIX 0x02
-#define BUF_SIZE 8
-
-// https://www.kernel.org/doc/htmldocs/usb/API-usb-control-msg.html
-#define REQUEST_TYPE 0x21       // ungetest
-#define REQUEST 0x09            // ungetest
-
-#define function(name, which) \
+#define function(name, cmdType) \
 	static ssize_t show_ ## name(struct device *dev, struct device_attribute *attr, char *buf) { \
 		struct usb_missile *missile = usb_get_intfdata(to_usb_interface(dev)); \
 		printk("%s\n", #name); \
-		if (which == Movement) { \
-			return sprintf(buf, "%d\n", missile->direction); \
-		} else if (which == Led) { \
-			return sprintf(buf, "%d\n", missile->led); \
+		if (cmdType == Movement) { \
+			return sprintf(buf, "Last Movement: %d\n",missile->direction); \
+		} else if (cmdType == Led) { \
+			return sprintf(buf, "LED Status: %d\n", missile->led); \
 		} else { \
-			return sprintf(buf, "%d\n", 1); \
+			return sprintf(buf, "Nothing to read\n"); \
 		} \
 	} \
 	static ssize_t store_ ## name(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) { \
 		struct usb_missile *missile = usb_get_intfdata(to_usb_interface(dev)); \
-		if (which == Movement) { \
+		if (cmdType == Movement) { \
 			missile->direction = name; \
-		} else if (which == Led) { \
+		} else if (cmdType == Led) { \
 			missile->led = name; \
-		} else { \
-			missile->direction = Stop; \
-			missile->led = Off; \
 		} \
-		execute_order(missile, which); \
+		execute_order(missile, cmdType); \
 		return count; \
 	} \
 	static DEVICE_ATTR(name, S_IRUSR | S_IWUSR, show_ ## name, store_ ## name);
@@ -52,9 +41,9 @@
 
 // TODO mögliche Fehler abfangen
 #define create(name) device_create_file(&interface->dev, &dev_attr_ ## name)
-#define create_all  create(Stop); create(Down); create(Up); create(Left); create(Right); create(UpLeft); create(UpRight); create(DownLeft); create(DownRight); create(Fire); create(On); create(Off)
+#define create_all_devices()  create(Stop); create(Down); create(Up); create(Left); create(Right); create(UpLeft); create(UpRight); create(DownLeft); create(DownRight); create(Fire); create(LedOn); create(LedOff)
 #define remove(name) device_remove_file(&interface->dev, &dev_attr_ ## name)
-#define remove_all remove(Stop); remove(Down); remove(Up); remove(Left); remove(Right); remove(UpLeft); remove(UpRight);  remove(DownLeft); remove(DownRight); remove(Fire); remove(On); remove(Off)
+#define remove_all_devices() remove(Stop); remove(Down); remove(Up); remove(Left); remove(Right); remove(UpLeft); remove(UpRight);  remove(DownLeft); remove(DownRight); remove(Fire); remove(LedOn); remove(LedOff)
 
 static struct usb_device_id id_table[] = {
 	{ USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
@@ -77,8 +66,8 @@ enum Direction {
 
 
 enum Led {
-	Off,
-	On
+	LedOff,
+	LedOn
 };
 
 
@@ -88,45 +77,48 @@ struct usb_missile {
 	enum Led		led;
 };
 
-enum Which {
+enum CmdType {
 	Led,
 	Movement,
 	Fire
 };
 
-static void execute_order(struct usb_missile *missile, enum Which which)
+static void execute_order(struct usb_missile *missile, enum CmdType cmdType)
 {
 	int result = 0;
 	unsigned char *buf = NULL;
-        // TODO müssen gucken ob das stimmt, pylibusb macht das so und auch der frontend-launcher
 	int value = 0;
 	int index = 0;
+	int buf_size = 8;
+	int request = 0x09;
+	int request_type = 0x21;
+	int mv_prefix = 0x02;
+	int led_prefix = 0x03;
 
-	buf = kzalloc(BUF_SIZE, GFP_KERNEL);
+	buf = kzalloc(buf_size, GFP_KERNEL);
 	if (buf == NULL) {
-		dev_err(&missile->udev->dev, "OOM");
+		dev_err(&missile->udev->dev, "Out of Memory\n");
 		return;
 	}
 
-	switch (which) {
+	switch (cmdType) {
 	case Movement:
-		buf[0] = MV_PREFIX;
+		buf[0] = mv_prefix;
 		buf[1] = missile->direction;
 		break;
 	case Led:
-		buf[0] = LED_PREFIX;
+		buf[0] = led_prefix;
 		buf[1] = missile->led;
 		break;
 	case Fire:
-		buf[0] = MV_PREFIX;
+		buf[0] = mv_prefix;
 		buf[1] = 0x10;
-        break;
+		break;
 	default:
 		break;
 	}
 
-        // TODO richtiges timeout noch herausfinden
-	result = usb_control_msg(missile->udev, usb_sndctrlpipe(missile->udev, 0), REQUEST, REQUEST_TYPE, value, index, buf, BUF_SIZE, 2 * HZ);
+	result = usb_control_msg(missile->udev, usb_sndctrlpipe(missile->udev, 0), request, request_type, value, index, buf, buf_size, 2 * HZ);
 	if (result < 0)
 		dev_err(&missile->udev->dev, "could not send data, direction: %d, led: %d\n", missile->direction, missile->led);
 
@@ -146,33 +138,32 @@ movement(Stop);
 
 action(Fire);
 
-led(On);
-led(Off);
+led(LedOn);
+led(LedOff);
 
 
 static int missile_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
 	struct usb_device *udev = interface_to_usbdev(interface);
 	struct usb_missile *dev = NULL;
-	int result = -ENOMEM;
+	int result = 0;
 
 	dev = kzalloc(sizeof(struct usb_missile), GFP_KERNEL);
 	if (dev == NULL) {
-		dev_err(&interface->dev, "OOM\n");
+		dev_err(&interface->dev, "Out of Memory\n");
+		result = -ENOMEM;
 		goto error;
 	}
 
 	dev->udev = usb_get_dev(udev);
 	usb_set_intfdata(interface, dev);
-        // Alle benötigen Devices erstellen
-	create_all;
+
+	create_all_devices();
 
 	dev_info(&interface->dev, "Connected\n");
 	return 0;
 
 error:
-	dev_info(&interface->dev, "Error\n");
-        // Ist das wirklich notwendig?
 	kfree(dev);
 	return result;
 }
@@ -185,8 +176,7 @@ static void missile_disconnect(struct usb_interface *interface)
 	dev = usb_get_intfdata(interface);
 	usb_set_intfdata(interface, NULL);
 
-        // Alle Devices wieder löschen
-	remove_all;
+	remove_all_devices();
 
 	usb_put_dev(dev->udev);
 	kfree(dev);
